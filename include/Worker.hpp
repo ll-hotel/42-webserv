71f0b50d4fc20ac939aa6963b804db6c8de22da7
gcros <guilhem.cros.38@gmail.com>
2024-12-03 20:47:43 +0100

    base

--- include/Worker.hpp
+++ include/Worker.hpp
@@ -1,16 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Worker.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/03 18:40:52 by gcros             #+#    #+#             */
-/*   Updated: 2024/12/03 18:44:12 by gcros            ###   ########.fr       */
+/*   Created: 2024/12/03 19:43:24 by gcros             #+#    #+#             */
+/*   Updated: 2024/12/03 20:08:26 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#ifndef WORKER_HPP
+# define WORKER_HPP
+
+class Worker
 {
-	return 0;
-}
+public:
+/*                                     constructor                            */
+
+	Worker();
+	~Worker();
+	Worker(Worker &worker) = delete;
+	Worker&	operator=(Worker &worker) = delete;
+};
+
+#endif
