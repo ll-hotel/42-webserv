297d738a3d7a4d0555c78e75937fdac70f380ec9
ll-hotel <150347935+ll-hotel@users.noreply.github.com>
2025-01-21 17:40:53 +0100

    Http request wrapper (#3)
    * feat: basic http request parser
    
    * enhancement: simplified http request wrapper
    
    * fix: just in case of const attributes
    
    ---------
    
    Co-authored-by: gcros <guilhem.cros.38@gmail.com>

---
+++ src/httpRequest/HttpRequest.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HttpRequest.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 20:08:58 by gcros             #+#    #+#             */
+/*   Updated: 2025/01/15 14:54:02 by gcros            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "webserv/HttpRequest.hpp"
+#include "webserv/Exception.hpp"
+#include <string>
+#include <sstream>
+#include <iostream>
+
+HttpRequest::HttpRequest()
+{
+}
+
+HttpRequest::HttpRequest(const HttpRequest &rp):
+_method(rp._method),
+_version(rp._version),
+_resources(rp._resources),
+_request(rp._request)
+{
+}
+
+HttpRequest& HttpRequest::operator=(const HttpRequest &other)
+{
+	_method = other._method;
+	_version = other._version;
+	_resources = other._resources;
+	_request = other._request;
+	return *this;
+}
+
+HttpRequest::HttpRequest(const std::string &request)
+{
+	std::istringstream stream_request(request);
+
+	if (request.empty() || !stream_request)
+		throw(WebservException("empty request"));
+	std::getline(stream_request, _method, ' ');
+	if (_method != "GET" && _method != "POST")
+		throw(WebservException("unsupported request method " + _method));
+	if (!stream_request)
+		throw(WebservException("incomplete request"));
+	std::getline(stream_request, _resources, ' ');
+	if (!stream_request)
+		throw(WebservException("incomplete request"));
+	std::getline(stream_request, _version, '\n');
+	if (_version != "HTTP/1.1\r")
+		throw(WebservException("unsupported HTTP version " + _version));
+	std::getline(stream_request, _request, '\0');
+}
+
+void HttpRequest::print()
+{
+	std::cout << this->_method
+		<< " " << this->_resources
+		<< " " << this->_version
+		<< std::endl;
+}
