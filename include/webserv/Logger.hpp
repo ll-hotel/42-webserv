07244af48cd629186d75fb9c90b4e78db08bcaf1
gcros <guilhem.cros.38@gmail.com>
2025-02-25 11:20:01 +0100

    feat basic logger system

---
+++ include/webserv/Logger.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Logger.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 15:08:44 by gcros             #+#    #+#             */
+/*   Updated: 2025/02/25 10:47:57 by gcros            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LOGGER_HPP
+# define LOGGER_HPP
+
+#include <string>
+#include <ctime>
+#include <queue>
+#include <fstream>
+
+class Logger
+{
+public:
+	Logger();
+	Logger(const std::string&);
+	~Logger();
+
+	enum e_log_level
+	{
+		INFO,
+		DEBUG,
+		WARN,
+		ERROR,
+		LOG_LEVEL_LEN,
+	};
+
+	void	log(Logger::e_log_level level, const std::string& message);
+	void	flush();
+	
+	struct s_log_object
+	{
+		char			str_time[20];
+		std::string		message;
+		enum e_log_level	type;
+	};
+
+private:
+	std::queue<struct s_log_object> _messageQueue;
+	Logger(Logger&);
+	Logger& operator=(Logger&);
+
+	std::ofstream	_outFile;
+};
+
+std::ostream &operator<<(std::ostream &os, const Logger::s_log_object &log_object);
+
+#endif
---
+++ src/logger/Logger.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Logger.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/25 09:59:48 by gcros             #+#    #+#             */
+/*   Updated: 2025/02/25 11:18:21 by gcros            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "webserv/Logger.hpp"
+#include <sstream>
+#include <iostream>
+
+Logger::Logger()
+{
+	this->_outFile.exceptions(this->_outFile.badbit);
+	this->_outFile.open("log.txt", std::fstream::out | std::fstream::app );
+	this->log(Logger::INFO, "---start log---");
+	this->flush();
+}
+
+Logger::Logger(const std::string &file_name)
+{
+	this->_outFile.exceptions(this->_outFile.badbit);
+	this->_outFile.open(file_name.c_str(), std::fstream::out | std::fstream::app );
+	this->log(Logger::INFO, "---start log---");
+	this->flush();
+}
+
+Logger::~Logger()
+{
+	this->_outFile.close();
+}
+
+void Logger::log(Logger::e_log_level level, const std::string &message)
+{
+	struct Logger::s_log_object log_object = {0};
+	time_t raw_time;
+	tm* timeinfo;
+
+	log_object.type = level;
+	log_object.message = message;
+	raw_time = time(0);
+	timeinfo = localtime(&raw_time);
+	strftime(log_object.str_time, 20, "%Y-%m-%d %H:%M:%S", timeinfo);
+	this->_messageQueue.push(log_object);
+}
+
+static std::string level_to_string(Logger::e_log_level level)
+{
+	std::string level_list[] = {
+		"INFO",
+		"DEBUG",
+		"WARN",
+		"ERROR",
+		"UNKOWN"
+	};
+
+	if (level >= Logger::LOG_LEVEL_LEN)
+		return (level_list[Logger::LOG_LEVEL_LEN]);
+	return (level_list[level]);
+}
+
+void Logger::flush()
+{
+	while (this->_messageQueue.size() > 0)
+	{
+		Logger::s_log_object log_object = this->_messageQueue.front();
+		this->_messageQueue.pop();
+		std::cout << log_object;
+		this->_outFile << log_object;
+	}
+}
+
+std::ostream &operator<<(std::ostream &os, const Logger::s_log_object &log_object)
+{
+	std::ostringstream log_entry;
+
+	log_entry << "[" << log_object.str_time << "] "
+		<< level_to_string(log_object.type)
+		<< ": " << log_object.message
+		<< std::endl;
+	os << log_entry.str();
+	return (os);
+}
